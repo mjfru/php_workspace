703cadf16c7f6bc8dc1352df722663ec68024b1c
Matt Frutchey <mjfru201@gmail.com>
2025-09-25 14:13:17 -0400

    initialize and finish notes and examples about nested loops in PHP

---
+++ notes/07nested_loops.php
@@ -0,0 +1,57 @@
+//! Nested Loops in PHP
+<?php
+// for($i = 0; $i < 5; $i++) {
+//   for($j = 0; $j < 5; $j++) {
+//     echo $i . ' - ' . $j . '</br>';
+//   }
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//   $j = 0;
+//   while ($j < 5) {
+//     echo $i . ' - ' . $j . '</br>';
+//     $j++;
+//   }
+//   $i++;
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    .grid-container {
+      display: grid;
+      grid-template-columns: repeat(5, 50px);
+      grid-gap: 5px;
+    }
+
+    .grid-item {
+      width: 50px;
+      height: 50px;
+      background-color: teal;
+      color: #fff;
+      text-align: center;
+      line-height: 50px;
+    }
+  </style>
+  <title>Nested Loops</title>
+</head>
+
+<body>
+  <div class="grid-container">
+    <?php for ($i = 0; $i < 5; $i++): ?>
+      <?php for ($j = 0; $j < 5; $j++): ?>
+        <div class="grid-item">
+          <?= $i . ' - ' . $j ?>
+        </div>
+      <?php endfor ?>
+    <?php endfor ?>
+  </div>
+</body>
+
+</html>
