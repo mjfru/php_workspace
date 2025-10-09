ee26d5e5dbe117fe9041cb8617274e6f0b354ef9
Matt Frutchey <mjfru201@gmail.com>
2025-10-09 10:20:19 -0400

    add example of 'the famous PHP loop' to the general loop note page

--- notes/06loops.php
+++ notes/06loops.php
@@ -92,3 +92,11 @@
 </body>
 
 </html>
+
+<!-- The famous WordPress 'while' loop: -->
+<?php while (have_posts()) {
+  the_post(); ?>
+  <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+  <?php the_content(); ?>
+  <hr>
+<?php }
