0289c5ea6835c6bdf12aae3b85af2cab974aa24c
Matt Frutchey <mjfru201@gmail.com>
2025-10-15 11:44:47 -0400

    begin oop notes in php

---
+++ notes/13oop.php
@@ -0,0 +1,10 @@
+<!-- OOP in PHP -->
+
+<!-- 
+Key Concepts to Remember: 
+  Objects = Self-contained unit that encapsulates properties and methods
+  Classes = Blueprints for an object
+  Inheritance = Allows one class to inherit properties/methods from another
+  Encapsulation = Bundling the data and methods into a single unit
+  Polymorphism = Allows objects of different classes to be treated as objects of a common superclass
+-->
