eb3d6e9d65654a3cfa6675fa91be4122e2bbd86d
Matt Frutchey <mjfru201@gmail.com>
2025-10-16 00:41:36 -0400

    finish current notes with infomation on access modifiers in php

--- notes/13oop.php
+++ notes/13oop.php
@@ -17,11 +17,24 @@ class User
   //? Public is for visibility of the property, can be accessed from outside of the class.
   public $name;
   public $email;
+  private $status = 'active';
 
   public function __construct($name, $email)
   {
     $this->name = $name;
     $this->email = $email;
+  }
+
+  //* Although $status is private, we can access it with a public function: 
+  //* Getter function:
+  public function getStatus()
+  {
+    echo $this->status;
+  }
+
+  //* Setter function
+  public function setStatus($status) {
+    $this->status = $status;
   }
 
   //? Methods Reminder - Functions that belong to a class.
@@ -40,7 +53,18 @@ $user1 = new User('John Doe', 'john@gmail.com');
 
 // var_dump($user1);
 $user1->login();
+$user1->setStatus('inactive');
+$user1->getStatus();
 
 $user2 = new User('Jane Doe', 'jane@gmail.com');
 // var_dump($user1);
 ?>
+
+<!-- 
+Access Modifiers:
+    - public
+      The property or method can be accessed from anywhere; the default.
+    - protected
+      The property or method can be accessed from within the class OR any class that inherits it
+    - private
+      The property can ONLY be accessed from within the class.
