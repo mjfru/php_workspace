05f41ff4650e49975f5f9abbddd6e880a18e1bb7
Matt Frutchey <mjfru201@gmail.com>
2025-10-17 07:12:38 -0400

    update oop notes to include how to use abstract classes and interfaces in php

--- notes/14oop2.php
+++ notes/14oop2.php
@@ -73,5 +73,121 @@ class MathUtility
 //* We can make it a static property / method and access it directly from the class: 
 // Using the scope resolution operator:
 echo MathUtility::$pi;
-echo MathUtility::add(1,2,3,4,5);
+echo MathUtility::add(1, 2, 3, 4, 5);
+
+//! Abstract Classes
+abstract class Shape
+{
+  protected $name;
+
+  abstract public function calculateArea();
+
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
+
+  // Concrete Methods
+  public function getName()
+  {
+    return $this->name;
+  }
+}
+
+// If you extend from an abstract, you must implement the abstract methods too
+class Circle extends Shape
+{
+  private $radius;
+  public function __construct($name, $radius)
+  {
+    parent::__construct($name);
+    $this->radius = $radius;
+  }
+
+  public function calculateArea()
+  {
+    return pi() * pow($this->radius, 2);
+  }
+}
+
+class Rectangle extends Shape
+{
+  private $width;
+  private $height;
+
+  public function __construct($name, $height, $width)
+  {
+    parent::__construct($name);
+    $this->height = $height;
+    $this->width = $width;
+  }
+
+  public function calculateArea()
+  {
+    return $this->width * $this->height;
+  }
+}
+
+$circle = new Circle('Circle', 5);
+var_dump($circle);
+
+$rectangle = new Rectangle('Rectangle', 4, 6);
+echo $circle->getName() . ' Area: ' . $circle->calculateArea() . '<br>';
+echo $rectangle->getName() . ' Area: ' . $rectangle->calculateArea() . '<br>';
+
+//! Interfaces - A 'contract' for classes
+interface ContentInterface
+{
+  public function display();
+  public function edit();
+}
+
+class Article implements ContentInterface
+{
+  private $title;
+  private $content;
+
+  public function __construct($title, $content)
+  {
+    $this->title = $title;
+    $this->content = $content;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<p>{$this->content}</p>";
+  }
+
+  public function edit()
+  {
+    echo "Editing this article '{$this->title}'";
+  }
+}
+
+class Video implements ContentInterface
+{
+  private $title;
+  private $url;
+
+  public function __construct($title, $url)
+  {
+    $this->title = $title;
+    $this->url = $url;
+  }
+
+  public function display()
+  {
+    echo "<h2>{$this->title}</h2>";
+    echo "<iframe src='{$this->url}'></iframe>";
+  }
+
+  public function edit()
+  {
+    echo "Editing this video: '{$this->title}'";
+  }
+}
+
+$article = new Article('Introduction to PHP', 'PHP is a versatile scripting language...');
+$video = new Video('PHP for All', 'https://udemy.com');
 ?>
