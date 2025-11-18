861aee1d18fc4034337e78b32509d43e09edb441
Matt Frutchey <mjfru201@gmail.com>
2025-11-18 12:30:41 -0500

    create a single post page for users to navigate to, use a prepared statement for select the correct post

--- demos/mysql_php_demo/index.php
+++ demos/mysql_php_demo/index.php
@@ -36,7 +36,10 @@ $posts = $statement->fetchAll();
       <div class="md my-4">
         <div class="rounded-lg shadow-md">
           <div class="p-4">
-            <h2 class="text-xl font-semibold"><?= $post['title'] ?></h2>
+            <h2 class="text-xl font-semibold">
+              <a href="post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+
+            </h2>
             <p class="text-gray-700 text-lg mt-2"><?= $post['body'] ?></p>
           </div>
         </div>
