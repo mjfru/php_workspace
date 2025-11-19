531087b6233a52ac449ad05f35c3a0699a89b226
Matt Frutchey <mjfru201@gmail.com>
2025-11-19 11:47:25 -0500

    create functionality added with new html template and php therein

--- demos/mysql_php_demo/index.php
+++ demos/mysql_php_demo/index.php
@@ -45,6 +45,12 @@ $posts = $statement->fetchAll();
         </div>
       </div>
     <?php endforeach ?>
+    
+    <div class="mt-6">
+      <a href="create.php" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
+        Create Post
+      </a>
+    </div>
   </div>
 </body>
 
