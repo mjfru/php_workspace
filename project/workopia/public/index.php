e7b9b69a1207ce1450a7e5d5298ec1dda1c4b802
Matt Frutchey <mjfru201@gmail.com>
2026-01-11 10:16:59 -0500

    update/add namespaces to relevants files, clean up errors

--- project/workopia/public/index.php
+++ project/workopia/public/index.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 require '../helpers.php';
+
+use Framework\Router;
+
 // require basePath('Framework/Router.php');
 // require basePath('Framework/Database.php');
 
