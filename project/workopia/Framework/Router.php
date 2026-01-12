b2535266d26706a4b4613240c391add79974c9ea
Matt Frutchey <mjfru201@gmail.com>
2026-01-12 06:45:31 -0500

    refactor router for controller classes

--- project/workopia/Framework/Router.php
+++ project/workopia/Framework/Router.php
@@ -15,12 +15,15 @@ class Router
 {
   protected $routes = [];
 
-  public function registerRoute($method, $uri, $controller)
+  public function registerRoute($method, $uri, $action)
   {
+    list($controller, $controllerMethod) = explode('@', $action);
+
     $this->routes[] = [
       'method' => $method,
       'uri' => $uri,
-      'controller' => $controller
+      'controller' => $controller,
+      'controllerMethod' => $controllerMethod
     ];
   }
 
@@ -65,8 +68,9 @@ class Router
    * @param int $httpCode
    * @return void
    */
-  public function error($httpCode = 404) {
-        http_response_code($httpCode);
+  public function error($httpCode = 404)
+  {
+    http_response_code($httpCode);
     loadView("error/{$httpCode}");
     exit;
   }
@@ -78,10 +82,17 @@ class Router
    * @param string $method
    * @return void
    */
-  public function route($uri, $method) {
-    foreach($this->routes as $route) {
-      if($route['uri'] === $uri && $route['method'] === $method) {
-        require basePath('App/' . $route['controller']);
+  public function route($uri, $method)
+  {
+    foreach ($this->routes as $route) {
+      if ($route['uri'] === $uri && $route['method'] === $method) {
+        // Extract controller & controller method
+        $controller = 'App\\Controllers\\' . $route['controller'];
+        $controllerMethod = $route['controllerMethod'];
+
+        // Instantiate the controller and call the method
+        $controllerInstance = new $controller();
+        $controllerInstance->$controllerMethod();
         return;
       }
     }
