07120e5b4941a1f8814843dddcf6965b25e2e286
Matt Frutchey <mjfru201@gmail.com>
2026-01-17 12:43:03 -0500

    begin work on validation class

---
+++ project/workopia/Framework/Validation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Framework;
+
+class Validation
+{
+  /**
+   * Validate a string
+   * 
+   * @param string $value
+   * @param int $min
+   * @param int $max
+   * @return bool
+   */
+
+  public static function string($value, $min = 1, $max = INF)
+  {
+    if (is_string($value)) {
+      $value = trim($value);
+      $length = strlen($value);
+      return $length >= $min && $length <= $max;
+    }
+
+    return false;
+  }
+}
