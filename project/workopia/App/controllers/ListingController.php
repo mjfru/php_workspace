6ef1395933ca62675f37ff797f9e846ae942eacf
Matt Frutchey <mjfru201@gmail.com>
2026-01-20 00:30:59 -0500

    successfully add functionality to add a new listing to the db, redirect home

--- project/workopia/App/controllers/ListingController.php
+++ project/workopia/App/controllers/ListingController.php
@@ -81,15 +81,31 @@ class ListingController
         'listing' => $newListingData,
       ]);
     } else {
-      // echo 'Success!';
+      // Writing to the DB:
       $fields = [];
 
-      foreach($newListingData as $field => $value) {
+      foreach ($newListingData as $field => $value) {
         $fields[] = $field;
       }
 
       $fields = implode(', ', $fields);
+      $values = [];
 
-    }
+      foreach ($newListingData as $field => $value) {
+        // Convert empty strings to null:
+        if ($value === "") {
+          $newListingData[$field] = null;
+        };
+        $values[] = ":" . $field;
+      };
+
+      $values = implode(", ", $values);
+
+      $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+      $this->db->query($query, $newListingData);
+
+      redirect('/listings');
+      exit;
+    };
   }
 }
--- project/workopia/helpers.php
+++ project/workopia/helpers.php
@@ -95,3 +95,15 @@ function sanitize($dirty)
 {
   return filter_var(trim($dirty), FILTER_SANITIZE_SPECIAL_CHARS);
 }
+
+/**
+ * Redirect to a given URL
+ * 
+ * @param string $url
+ * @return void
+ */
+
+function redirect($url)
+{
+  header("Location: {$url}");
+}
