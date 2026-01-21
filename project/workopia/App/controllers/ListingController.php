05f94548cea2e9da941e2ac1b073db7e1cd90ebc
Matt Frutchey <mjfru201@gmail.com>
2026-01-21 11:59:09 -0500

    conditionally render if tags are present or not for listings

--- project/workopia/App/controllers/ListingController.php
+++ project/workopia/App/controllers/ListingController.php
@@ -64,7 +64,7 @@ class ListingController
 
     $newListingData = array_map('sanitize', $newListingData);
 
-    $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+    $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state',];
 
     $errors = [];
 
