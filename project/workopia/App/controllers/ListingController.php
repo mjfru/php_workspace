a20d3aeee9ae26d461ab51b92a2db6870b3af75a
Matt Frutchey <mjfru201@gmail.com>
2026-01-22 11:43:08 -0500

    fix routes for RESTful consistency, implement delete/destroy functionality for listings

--- project/workopia/App/controllers/ListingController.php
+++ project/workopia/App/controllers/ListingController.php
@@ -28,6 +28,12 @@ class ListingController
     loadView('listings/create');
   }
 
+  /**
+   * Show a single listing
+   *
+   * @param array $params
+   * @return void
+   */
   public function show($params)
   {
     $id = $params['id'] ?? "";
@@ -107,5 +113,30 @@ class ListingController
       redirect('/listings');
       exit;
     };
+  }
+
+  /**
+   * Delete a listing
+   * 
+   * @param array $params
+   * @return void
+   */
+  public function destroy($params)
+  {
+    $id = $params['id'];
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    if (!$listing) {
+      ErrorController::notFound('Listing not found.');
+      return;
+    }
+
+    $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+    redirect('/listings');
   }
 }
